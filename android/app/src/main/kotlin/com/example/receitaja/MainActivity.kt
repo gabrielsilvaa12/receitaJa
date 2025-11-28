e53944697deba17a345ee417659dd7f623230b44
Gabriel <gabrieusilvaa14@gmail.com>
2025-11-28 19:37:18 -0300

    criação do repositório

---
+++ android/app/src/main/kotlin/com/example/receitaja/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.receitaja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:receitaja/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
